ae04c73f99c118b1139fda104d5f583445c4d787
shimakaze-git <shimakaze.soft+github@googlemail.com>
2022-02-03 11:32:11 +0900

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.4.19;
 
 contract ZombieFactory {
+
     uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+    
+    // ここにZombieというstructを定義するのだ
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 }
