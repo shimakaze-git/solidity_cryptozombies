f00adfb2759edc0fb0c9c7ff6229c23a5eaeadc8
shimakaze-git <shimakaze.soft+github@googlemail.com>
2022-02-03 11:41:17 +0900

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -4,10 +4,14 @@ contract ZombieFactory {
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
-    
-    // ここにZombieというstructを定義するのだ
+
     struct Zombie {
         string name;
         uint dna;
     }
+
+    Zombie[] public zombies;
+
+    // ここにcreateZombieという関数を定義するのだ
+
 }
